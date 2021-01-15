f72672d417c22f8aa87071b0f7191852c7c0a5fc
Marina Alane <marina.alane@outlook.com>
2021-01-15 11:59:46 -0300

    Exercicio com Arrow Function

---
+++ exercises/7.1/script.js
@@ -0,0 +1,18 @@
+// arrow functions e templates literals
+
+/* 1 Faça as modificações necessárias na função para que o seu comportamento respeite o escopo no qual cada variável foi declarada.
+Modifique a estrutura da função para que ela seja uma arrow function .
+Modifique as concatenações para template literals .
+Copie o código abaixo. */
+
+const testingScpe = (escopo) => { 
+  if (escopo === true) { 
+    var ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+    ifScope = ifScope + ' ótimo, fui utilizada no escopo !';
+    console.log(ifScope);
+  } else {
+    var elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+    console.log(elseScope);
+  }
+}
+ testingScpe(true);
