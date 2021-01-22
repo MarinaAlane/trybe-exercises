645ec41f9fbfc1871d27e30b725768fdd557cfb2
Marina Alane <marina.alane@outlook.com>
2021-01-22 16:23:57 -0300

    transforma funçao em arrow function

--- exercises/8.1/8.1.js
+++ exercises/8.1/8.1.js
@@ -65,9 +65,7 @@ const books = [
   },
 ];
 
-function authorBornIn1947 () {
-  return books.find(book => book.author.birthYear === 1947).author.name;
- 
-}
+const authorBornIn1947 = () => books.find(book => book.author.birthYear === 1947).author.name;
+
 console.log(authorBornIn1947())
 
