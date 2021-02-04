e07a9afee6fab6d072b2564b092133367b1d8c40
Marina Alane <marina.alane@outlook.com>
2021-02-04 13:07:20 -0300

    ex

---
+++ exercises/9.1/refatorando-test.js
@@ -0,0 +1,7 @@
+const { sum } = require('./refatorando')
+
+describe('Testa se a função retorna uma soma', () => {
+  test('A função soma os elementos', () => {
+    
+  })
+})
---
+++ exercises/9.1/refatorando.js
@@ -0,0 +1,9 @@
+// REFATORANDO PRIMEIROS EXERCÍCIOS DE TESTE USANDO JEST
+
+// A função sum(a, b) retorna a soma do parâmetro a com o b
+
+function sum (a, b){
+  return a + b;
+}
+
+
