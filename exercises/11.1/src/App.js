ab3881c0835ca5d3eb0e687b7c5638977fc7aa87
Marina Alane <marina.alane@outlook.com>
2021-02-24 09:34:10 -0300

    primeiro app react

---
+++ exercises/11.1/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div> 
+    <h1 className='OlaMundo'> Olá mundo ! </h1> 
+    <span> Meu primeiro app react</span>
+    </div>
+  );
+}
+
+export default App;
