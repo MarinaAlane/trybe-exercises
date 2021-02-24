8e93db4ae458d79d1feb59119cfda27af26c98f7
Marina Alane <marina.alane@outlook.com>
2021-02-24 10:44:28 -0300

    adiciona componentes

---
+++ exercises/11.1/src/HelloWorld.js
@@ -0,0 +1,12 @@
+import React from 'react ';
+import './App.css';
+
+class HelloWorld extends React.Component {
+  render () {
+    return (
+      <h1 className="OlaMundo"> Hello, world </h1>
+    )
+  }
+}
+
+export default HelloWorld;
